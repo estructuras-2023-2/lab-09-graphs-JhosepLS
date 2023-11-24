3bfaf7358c4e43993f1633487a24be0679e46bc7
JhosepLS <143119700+JhosepLS@users.noreply.github.com>
2023-11-24 18:15:21 -0500

    Clase UnionFind y clases para verificar si estan conectados todos o algunos, además de unir también.

--- student_file.h
+++ student_file.h
@@ -5,3 +5,37 @@
 #include <set>
 
 using namespace std;
+
+class UnionFind {
+private:
+    map<string, string> padre;
+
+public:
+    string buscar(const string& s) {
+        if (padre.find(s) == padre.end())
+            padre[s] = s;
+        else if (padre[s] != s)
+            padre[s] = buscar(padre[s]);
+        return padre[s];
+    }
+
+    void unir(const string& a, const string& b) {
+        string raizA = buscar(a);
+        string raizB = buscar(b);
+        if (raizA != raizB)
+            padre[raizA] = raizB;
+    }
+
+    bool estanConectados(const string& a, const string& b) {
+        return buscar(a) == buscar(b);
+    }
+
+    bool todosConectados(const vector<string>& ciudades) {
+        if (ciudades.empty()) return true;
+        string raiz = buscar(ciudades.front());
+        for (const auto& ciudad : ciudades) {
+            if (buscar(ciudad) != raiz) return false;
+        }
+        return true;
+    }
+};
