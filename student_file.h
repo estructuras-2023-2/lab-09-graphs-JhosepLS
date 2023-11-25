b5ec8292c309775b0269c5b68d6a8b61fed87820
JhosepLS <143119700+JhosepLS@users.noreply.github.com>
2023-11-24 19:24:47 -0500

    Creamos la función principal para reconstruir carreteras

--- student_file.h
+++ student_file.h
@@ -61,3 +61,39 @@ vector<string> dividir(const string& str, char delim) {
     tokens.push_back(str.substr(inicio));
     return tokens;
 }
+
+string reconstruye(vector<string> carreteras) {
+    vector<Carretera> todasCarreteras;
+    vector<string> todasCiudades;
+    UnionFind uf;
+    set<string> carreterasReconstruir;
+
+    for (const auto& carreteraStr : carreteras) {
+        auto tokens = dividir(carreteraStr, ' ');
+        todasCiudades.push_back(tokens[1]);
+        todasCiudades.push_back(tokens[2]);
+        if (tokens.size() == 4) {
+            todasCarreteras.emplace_back(tokens[0], tokens[1], tokens[2], stoi(tokens[3]));
+        } else {
+            uf.unir(tokens[1], tokens[2]);
+        }
+    }
+
+    for (const auto& carretera : todasCarreteras) {
+        if (!uf.estanConectados(carretera.ciudad1, carretera.ciudad2)) {
+            uf.unir(carretera.ciudad1, carretera.ciudad2);
+            carreterasReconstruir.insert(carretera.id);
+        }
+    }
+
+    if (!uf.todosConectados(todasCiudades)) {
+        return "IMPOSIBLE";
+    }
+
+    string resultado;
+    for (const auto& id : carreterasReconstruir) {
+        resultado += id + " ";
+    }
+
+    return resultado.empty() ? "" : resultado.substr(0, resultado.length() - 1);
+}
